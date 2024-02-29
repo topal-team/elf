0d81dafc4d7ba260b84a3a6fbe66ea54a9d1cca1
AGUILA-MULTNER Adrien <adrien.aguila-multner@inria.fr>
2024-02-29 10:28:54 +0100

    Better script

--- start.sh
+++ start.sh
@@ -2,17 +2,20 @@
 
 #SBATCH --constraint='sirocco'
 #SBATCH --job-name=multinode
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
 #SBATCH --exclude=sirocco[01-06,10-15,17,24-25]
 
-nodes=($( scontrol show hostnames $SLURM_JOB_NODELIST ))
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+if [[ $SLURM_NTASKS -gt 1 ]] ; then
+    nodes=($( scontrol show hostnames $SLURM_JOB_NODELIST ))
+    nodes_array=($nodes)
+    head_node=${nodes_array[0]}
+    head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 
-echo Head node ip : $head_node_ip $head_node
-
+    echo Head node ip : $head_node_ip $head_node
+    options="--rdzv-id $RANDOM --rdzv-backend c10d --rdzv-endpoint $head_node_ip:26500"
+else
+    options=""
+fi
+   
 srun singularity exec --nv /beegfs/aaguilam/images/nanotron.sif \
-torchrun --nnodes $SLURM_NNODES --nproc-per-node $SLURM_NTASKS_PER_NODE --rdzv-id $RANDOM --rdzv-backend c10d \
---rdzv-endpoint $head_node_ip:26500 $1
+torchrun --nnodes $SLURM_JOB_NUM_NODES --nproc-per-node $SLURM_NTASKS_PER_NODE \
+${options} $1
