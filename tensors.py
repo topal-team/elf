aa6759230fc5c8de11495a244952d64a9ec1ac13
AGUILA-MULTNER Adrien <adrien.aguila-multner@inria.fr>
2024-02-29 11:02:57 +0100

    Multi-node synchronous & asynchronous tensor passing

---
+++ tensors.py
@@ -0,0 +1,37 @@
+import torch
+import torch.distributed as dist
+import os
+
+if __name__ == "__main__":
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["LOCAL_RANK"])
+    global_rank = int(os.environ["RANK"])
+    
+    # Processes send to next rank and receive from previous one
+    src = (global_rank - 1) % world_size
+    dst = (global_rank + 1) % world_size
+
+    dist.init_process_group(backend="nccl", world_size=world_size)
+
+    # Synchronous
+    x = torch.randint(0, 10, (3,)).to(rank)
+    for r in range(world_size):
+        if r == global_rank:
+            print(f'[GPU {global_rank}] - Sending {x} synchronously')
+            dist.send(x, dst)
+        elif r == src:
+            y = torch.zeros_like(x).to(rank)
+            dist.recv(y, src)
+            print(f'[GPU {global_rank}] - Received {y} synchronously')
+
+    # Asynchronous
+    x = torch.randint(0, 10, (3,)).to(rank)
+    print(f'[GPU {global_rank}] - Sending {x} asynchronously')
+    dist.isend(x, dst)
+
+    y = torch.zeros_like(x).to(rank)
+    status = dist.irecv(y, src)
+    status.wait()
+    print(f'[GPU {global_rank}] - Received {y} asynchronously')
+
+    dist.destroy_process_group()
