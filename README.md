39addbf82c39ea8d0a68ae9fa2466c3e85f6b9b3
AGUILA-MULTNER Adrien <adrien.aguila-multner@inria.fr>
2024-02-29 09:52:25 +0100

    Proper sbatch file

---
+++ README.md
@@ -0,0 +1,8 @@
+## Script usage for PlaFRIM
+
+Use script ``start.sh`` to start multi-nodes tasks.\
+Usage:
+```
+sbatch --nodes=? --ntasks-per-node=? ./start.sh your_script.py
+```
+
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=multinode-example
+#SBATCH --constraint='sirocco'
+#SBATCH --job-name=multinode
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --exclude=sirocco[01-06,10-15,17,24-25]
@@ -10,10 +11,8 @@ nodes_array=($nodes)
 head_node=${nodes_array[0]}
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 
-echo Node ip : $head_node_ip $head_node
-# export LOGLEVEL=INFO
-# export NCCL_DEBUG=INFO
+echo Head node ip : $head_node_ip $head_node
 
 srun singularity exec --nv /beegfs/aaguilam/images/nanotron.sif \
-torchrun --nnodes 1 --nproc-per-node 2 --rdzv-id $RANDOM --rdzv-backend c10d \
---rdzv-endpoint $head_node_ip:26500 /home/aaguilam/tests/pipelined.py
+torchrun --nnodes $SLURM_NNODES --nproc-per-node $SLURM_NTASKS_PER_NODE --rdzv-id $RANDOM --rdzv-backend c10d \
+--rdzv-endpoint $head_node_ip:26500 $1
