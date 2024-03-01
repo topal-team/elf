05354a54ba1027b12d9a20b5fe36555db358ff07
AGUILA-MULTNER Adrien <adrien.aguila-multner@inria.fr>
2024-03-01 10:30:30 +0100

    Profiling for tensor sending

--- tensors.py
+++ tensors.py
@@ -1,19 +1,13 @@
 import torch
 import torch.distributed as dist
+import torch.profiler
 import os
 
-if __name__ == "__main__":
-    world_size = int(os.environ["WORLD_SIZE"])
-    rank = int(os.environ["LOCAL_RANK"])
-    global_rank = int(os.environ["RANK"])
-
-    torch.cuda.set_device(rank)
-
+def main(world_size, rank, global_rank):
     # Processes send to next rank and receive from previous one
     src = (global_rank - 1) % world_size
     dst = (global_rank + 1) % world_size
 
-    dist.init_process_group(backend="nccl", world_size=world_size)
 
     # Synchronous
     x = torch.randint(0, 10, (3,)).to(rank)
@@ -36,6 +30,23 @@ if __name__ == "__main__":
     status.wait()
     print(f'[GPU {global_rank}] - Received {y} asynchronously')
 
+if __name__ == "__main__":
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["LOCAL_RANK"])
+    global_rank = int(os.environ["RANK"])
+
+    torch.cuda.set_device(rank)
+
+    dist.init_process_group(backend="nccl", world_size=world_size)
+
+    with profiler.profile(
+            activities = [profiler.ProfilerActivity.CUDA],
+            profile_memory=True
+    ) as p:
+        main(world_size, rank, global_rank)
+
+    print(f'[GPU {global_rank}] :', p.key_averages().table(sort_by="self_cuda_time_total", row_limit=-1))
+    
     dist.barrier()
     if dist.is_initialized():
         dist.destroy_process_group()
