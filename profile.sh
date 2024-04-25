bef76eae3dda6897bfa5c38b032325e1dfea6d3c
Adrien Aguila--Multner <adrien.aguila-multner@inria.fr>
2024-04-25 15:20:31 +0200

    New profiling script

--- profile.sh
+++ profile.sh
@@ -5,7 +5,7 @@ export OMP_NUM_THREADS=1
 export TMPDIR=/gpfsscratch/rech/hyb/uak69sb/
 
 
-nsight="nsys profile -w true -t nvtx -s none --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace=true --trace-fork-before-exec true -f true -o /mnt/profiling/%p.nsys"
+nsight="nsys profile -w true -t nvtx -s none --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace all -f true -o /mnt/profiling/%p.nsys"
 run="torchrun --nnodes 1 --nproc-per-node 4 --standalone --no-python -- ${nsight} python /mnt/${@:1}"
 # run="python /mnt/${@:1}"
 
