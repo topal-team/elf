da6dbd01be9dada1c7ed2b904db8049d2f2d65ea
Adrien Aguila--Multner <adrien.aguila-multner@inria.fr>
2024-05-14 18:10:42 +0200

    profiling tools for hanayo

---
+++ profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+# See Jean-Zay doc on nsight systems
+export TMPDIR=/gpfsscratch/rech/hyb/uak69sb/
+
+rm profiling/* ;
+
+nsight="nsys profile -t cuda,nvtx --capture-range=cudaProfilerApi --capture-range-end=stop -f true -o profiling/%p.nsys"
+run="torchrun --nnodes 1 --nproc-per-node 4 --standalone --no-python -- ${nsight} python ${@:1}"
+# run="python /mnt/${@:1}"
+
+echo Executing : "${run}"
+singularity exec --nv --bind ${PWD}:${PWD},${TMPDIR}:${TMPDIR} ${SINGULARITY_ALLOWED_DIR}/pipe.sif \
+     bash -c "${run}"
