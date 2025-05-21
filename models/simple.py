9a0fab300fe87ada808a3a5218d0f557443d2058
Julia Gusak <julgusak@gmail.com>
2025-05-21 11:23:31 +0200

    FlashAttention & FP16 Transformers (#56)
    * Add flash attention via torch.nn.functional.scaled_dot_product_attention
    
    * Add sdp_backend option
    
    * Apply automatic formatting and linting fixes
    
    ---------
    
    Co-authored-by: juliagusak <juliagusak@users.noreply.github.com>

--- models/simple.py
+++ models/simple.py
@@ -2,6 +2,8 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.attention import SDPBackend, sdpa_kernel
+# Fused kernels (Flash, Mem-Efficient, CuDNN) require float16 or bfloat16
 
 """
 This is a model zoo for tests and benchmarks.
@@ -153,9 +155,9 @@ class SimpleAttention(nn.Module):
 
 	def forward(self, inputs):
 		# Linear projections
-		Q = self.query(inputs)
-		K = self.key(inputs)
-		V = self.value(inputs)
+		Q = self.query(inputs).unsqueeze(1)
+		K = self.key(inputs).unsqueeze(1)
+		V = self.value(inputs).unsqueeze(1)
 
 		# Compute attention scores
 		scores = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(
@@ -180,13 +182,48 @@ class SimpleAttention(nn.Module):
 		return torch.nn.functional.mse_loss(pred, target, *args, **kwargs)
 
 
+class SimpleFastAttention(nn.Module):
+	"""
+	Simple attention layer with a single head.
+	sdp_backend: can be 'FLASH_ATTENTION' or 'MATH'
+	"""
+
+	def __init__(self, hidden_dim, sdp_backend="FLASH_ATTENTION"):
+		super(SimpleFastAttention, self).__init__()
+		self.hidden_dim = hidden_dim
+
+		self.query = nn.Linear(hidden_dim, hidden_dim)
+		self.key = nn.Linear(hidden_dim, hidden_dim)
+		self.value = nn.Linear(hidden_dim, hidden_dim)
+		self.softmax = nn.Softmax(dim=-1)
+
+	def forward(self, inputs):
+		# Linear projections
+		Q = self.query(inputs).unsqueeze(1)
+		K = self.key(inputs).unsqueeze(1)
+		V = self.value(inputs).unsqueeze(1)
+		with sdpa_kernel(backends=[SDPBackend.__dict__[sdp_backend]]):
+			context = F.scaled_dot_product_attention(Q, K, V).squeeze(1)
+			return context
+
+	def get_sample(self, batch_size):
+		return torch.randn(batch_size, 64, self.hidden_dim)
+
+	def get_target(self, batch_size):
+		return self.get_sample(batch_size)  # same
+
+	def loss_fn(self, pred, target, *args, **kwargs):
+		return torch.nn.functional.mse_loss(pred, target, *args, **kwargs)
+
+
 class SimpleTransformer(nn.Module):
 	"""
 	Simple transformer with a single head.
 	ffn_dim is optional and defaults to hidden_dim * 4.
+	sdp_backend can be 'FLASH_ATTENTION', 'MATH' or None
 	"""
 
-	def __init__(self, input_dim, hidden_dim, n_blocks=4, seq_len=64, ffn_dim=None):
+	def __init__(self, input_dim, hidden_dim, n_blocks=4, seq_len=64, ffn_dim=None, sdp_backend=None):
 		super(SimpleTransformer, self).__init__()
 
 		ffn_dim = ffn_dim or hidden_dim * 4
@@ -199,18 +236,29 @@ class SimpleTransformer(nn.Module):
 		self.head = nn.Linear(hidden_dim, input_dim)
 		self.blocks = []
 		for i in range(n_blocks):
-			self.blocks.append(
-				nn.Sequential(
-					SimpleAttention(hidden_dim),
-					nn.LayerNorm(hidden_dim),
-					FeedForward(hidden_dim, ffn_dim),
-					nn.LayerNorm(hidden_dim),
+			if sdp_backend is not None:
+				self.blocks.append(
+					nn.Sequential(
+						SimpleFastAttention(hidden_dim, sdp_backend),
+						nn.LayerNorm(hidden_dim),
+						FeedForward(hidden_dim, ffn_dim),
+						nn.LayerNorm(hidden_dim),
+					)
 				)
-			)
+			else:
+				self.blocks.append(
+					nn.Sequential(
+						SimpleAttention(hidden_dim),
+						nn.LayerNorm(hidden_dim),
+						FeedForward(hidden_dim, ffn_dim),
+						nn.LayerNorm(hidden_dim),
+					)
+				)
 			self.add_module(f"block_{i}", self.blocks[-1])
 
 	def forward(self, x):
 		x = self.embed(x)
+		x = x.to(self.head.weight.dtype)
 
 		for b in self.blocks:
 			x = b(x)
@@ -264,12 +312,25 @@ class Attention(nn.Module):
 		return torch.matmul(attn, v)
 
 
+class FastAttention(nn.Module):
+	def __init__(self, dim, dropout=0.1, sdp_backend="FLASH_ATTENTION"):
+		super().__init__()
+		self.dim = dim
+		self.scale = 1.0 / math.sqrt(self.dim)
+		self.dropout = nn.Dropout(dropout)
+
+	def forward(self, q, k, v):
+		with sdpa_kernel(backends=[SDPBackend.__dict__[sdp_backend]]):
+			context = F.scaled_dot_product_attention(q, k, v)
+			return context
+
+
 class MultiHeadAttention(nn.Module):
 	"""
 	Util for transformers.
 	"""
 
-	def __init__(self, dim, num_heads=4, dropout=0.1):
+	def __init__(self, dim, num_heads=4, dropout=0.1, sdp_backend="FLASH_ATTENTION"):
 		super().__init__()
 		assert dim % num_heads == 0, "dim must be divisible by num_heads"
 
@@ -281,7 +342,10 @@ class MultiHeadAttention(nn.Module):
 		self.k_proj = nn.Linear(dim, dim)
 		self.v_proj = nn.Linear(dim, dim)
 
-		self.attn = Attention(self.head_dim, dropout)
+		if sdp_backend is not None:
+			self.attn = FastAttention(self.head_dim, dropout, sdp_backend=sdp_backend)
+		else:
+			self.attn = Attention(self.head_dim, dropout)
 
 	def forward(self, x):
 		batch_size, seq_len, _ = x.shape
@@ -303,17 +367,26 @@ class MultiHeadAttention(nn.Module):
 
 		return out
 
+	def get_sample(self, batch_size):
+		return torch.randn(batch_size, 64, self.hidden_dim)
+
+	def get_target(self, batch_size):
+		return self.get_sample(batch_size)  # same
+
+	def loss_fn(self, pred, target, *args, **kwargs):
+		return torch.nn.functional.mse_loss(pred, target, *args, **kwargs)
+
 
 class TransformerBlock(nn.Module):
 	"""
 	Util for transformers.
 	"""
 
-	def __init__(self, dim, num_heads=4, dropout=0.1, ffn_dim=None):
+	def __init__(self, dim, num_heads=4, dropout=0.1, ffn_dim=None, sdp_backend=None):
 		super().__init__()
 		self.norm1 = nn.LayerNorm(dim)
 		self.norm2 = nn.LayerNorm(dim)
-		self.attn = MultiHeadAttention(dim, num_heads, dropout)
+		self.attn = MultiHeadAttention(dim, num_heads, dropout, sdp_backend)
 		self.proj = nn.Linear(dim, dim)
 		self.dropout1 = nn.Dropout(dropout)
 		self.dropout2 = nn.Dropout(dropout)
@@ -345,7 +418,15 @@ class FullTransformer(nn.Module):
 	"""
 
 	def __init__(
-		self, input_dim, hidden_dim, n_blocks=4, seq_len=64, num_heads=4, dropout=0.1, ffn_dim=None
+		self,
+		input_dim,
+		hidden_dim,
+		n_blocks=4,
+		seq_len=64,
+		num_heads=4,
+		dropout=0.1,
+		ffn_dim=None,
+		sdp_backend=None,
 	):
 		super(FullTransformer, self).__init__()
 
@@ -359,11 +440,13 @@ class FullTransformer(nn.Module):
 		self.head = nn.Linear(hidden_dim, input_dim)
 		self.blocks = []
 		for i in range(n_blocks):
-			self.blocks.append(TransformerBlock(hidden_dim, num_heads, dropout, ffn_dim))
+			self.blocks.append(TransformerBlock(hidden_dim, num_heads, dropout, ffn_dim, sdp_backend))
 			self.add_module(f"block_{i}", self.blocks[-1])
 
 	def forward(self, x):
 		x = self.embed(x)
+
+		x = x.to(self.head.weight.dtype)
 
 		for b in self.blocks:
 			x = b(x)
@@ -389,7 +472,16 @@ class ChainTransformer(nn.Module):
 	ffn_dim is optional and defaults to hidden_dim * 4.
 	"""
 
-	def __init__(self, hidden_dim, n_blocks=4, seq_len=64, num_heads=4, dropout=0.1, ffn_dim=None):
+	def __init__(
+		self,
+		hidden_dim,
+		n_blocks=4,
+		seq_len=64,
+		num_heads=4,
+		dropout=0.1,
+		ffn_dim=None,
+		sdp_backend=None,
+	):
 		super(ChainTransformer, self).__init__()
 
 		ffn_dim = ffn_dim or hidden_dim * 4
@@ -398,7 +490,7 @@ class ChainTransformer(nn.Module):
 		self.seq_len = seq_len
 		self.blocks = []
 		for i in range(n_blocks):
-			self.blocks.append(TransformerBlock(hidden_dim, num_heads, dropout, ffn_dim))
+			self.blocks.append(TransformerBlock(hidden_dim, num_heads, dropout, ffn_dim, sdp_backend))
 			self.add_module(f"block_{i}", self.blocks[-1])
 
 	def forward(self, x):
@@ -414,3 +506,87 @@ class ChainTransformer(nn.Module):
 
 	def loss_fn(self, pred, target, *args, **kwargs):
 		return torch.nn.functional.mse_loss(pred, target, *args, **kwargs)
+
+
+if __name__ == "__main__":
+	import sys
+
+	sys.path.append("../")
+	from elf.utils import TimerGPU
+
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+	sdp_backend = "FLASH_ATTENTION"
+	if sdp_backend in ["FLASH_ATTENTION", "MATH"]:
+		dtype = torch.bfloat16
+	else:
+		dtype = torch.float32
+
+	batch_size = 16
+	num_heads = 1
+	seq_len = 1024
+	head_dim = 64
+	n_blocks = 4
+	embed_dim = num_heads * head_dim
+	hidden_dim = embed_dim
+
+	model = (
+		FullTransformer(
+			input_dim=embed_dim,
+			hidden_dim=hidden_dim,
+			n_blocks=n_blocks,
+			seq_len=seq_len,
+			num_heads=num_heads,
+			sdp_backend=sdp_backend,
+		)
+		.to(device)
+		.to(dtype)
+	)
+
+	# g = torch.fx.symbolic_trace(model).graph
+	# g.print_tabular()
+
+	sample = model.get_sample(batch_size).to(device)  # keep as long for embedding
+	target = model.get_target(batch_size).to(device)
+
+	for _ in range(3):
+		output = model(sample)
+		loss = model.loss_fn(output, target)
+		loss.backward()
+
+	with TimerGPU() as timer:
+		for _ in range(10):
+			output = model(sample)
+			loss = model.loss_fn(output, target)
+			loss.backward()
+	print(f"Forward and backward pass completed through FullTransformer Completed!: {timer.time()}")
+
+	model = (
+		ChainTransformer(
+			hidden_dim=hidden_dim,
+			n_blocks=n_blocks,
+			seq_len=seq_len,
+			num_heads=num_heads,
+			sdp_backend=sdp_backend,
+		)
+		.to(device)
+		.to(dtype)
+	)
+
+	# g = torch.fx.symbolic_trace(model).graph
+	# g.print_tabular()
+
+	sample = model.get_sample(batch_size).to(device).to(dtype)  # keep as long for embedding
+	target = model.get_target(batch_size).to(device).to(dtype)
+
+	for _ in range(3):
+		output = model(sample)
+		loss = model.loss_fn(output, target)
+		loss.backward()
+
+	with TimerGPU() as timer:
+		for _ in range(10):
+			output = model(sample)
+			loss = model.loss_fn(output, target)
+			loss.backward()
+	print(f"Forward and backward pass completed: {timer.time()}")
