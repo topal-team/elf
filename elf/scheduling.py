bda67d8c1b44187cd03f64d86ad8be8c8b2979bf
Adrien Aguila--Multner <adrien.aguila@gmail.com>
2025-06-05 14:18:39 +0200

    Removed old comm batching code

--- elf/scheduling.py
+++ elf/scheduling.py
@@ -5,7 +5,6 @@ Manipulate dependency graphs corresponding to schedules
 from enum import Enum, StrEnum, auto
 
 import logging
-import torch.distributed as dist
 
 logger = logging.getLogger("scheduling")
 
@@ -34,6 +33,24 @@ class OperationType(Enum):
 
 	def __str__(self) -> str:
 		return self.name.lower()
+
+
+comm_types = {
+	OperationType.RECV_FORWARD,
+	OperationType.RECV_BACKWARD,
+	OperationType.SEND_FORWARD,
+	OperationType.SEND_BACKWARD,
+}
+
+compute_types = {
+	OperationType.FORWARD,
+	OperationType.BACKWARD_INPUTS,
+	OperationType.BACKWARD_PARAMS,
+	OperationType.LOSS_FORWARD,
+	OperationType.LOSS_BACKWARD,
+	OperationType.RECOMPUTE_FORWARD,
+	OperationType.RECOMPUTE_BACKWARD_INPUTS,
+}
 
 
 class OpOptions(StrEnum):  # will be used as a key in a dict, needs to be a string
@@ -97,18 +114,6 @@ class Operation:
 		return hash((self.block_id, self.mb_id, self.rank, self.op))
 
 
-def complementary(op_type):
-	"""
-	The complementary is the opposite communication, that has the same peer.
-	"""
-	return {
-		OperationType.RECV_FORWARD: (OperationType.SEND_BACKWARD, OperationType.SEND_FORWARD),
-		OperationType.RECV_BACKWARD: (OperationType.SEND_FORWARD, OperationType.SEND_BACKWARD),
-		OperationType.SEND_FORWARD: (OperationType.RECV_BACKWARD, OperationType.RECV_FORWARD),
-		OperationType.SEND_BACKWARD: (OperationType.RECV_FORWARD, OperationType.RECV_BACKWARD),
-	}[op_type]
-
-
 def matching(op_type):
 	"""
 	The matching operation is the opposite communication, in the same direction.
@@ -120,80 +125,6 @@ def matching(op_type):
 		OperationType.SEND_FORWARD: OperationType.RECV_FORWARD,
 		OperationType.SEND_BACKWARD: OperationType.RECV_BACKWARD,
 	}[op_type]
-
-
-comm_types = {
-	OperationType.RECV_FORWARD,
-	OperationType.RECV_BACKWARD,
-	OperationType.SEND_FORWARD,
-	OperationType.SEND_BACKWARD,
-}
-
-
-def resolve_one_pair(ops1, ops2):
-	"""
-	Find the blocking pattern between a pair of ranks
-	"""
-	if len(ops1) == 0 or len(ops2) == 0:
-		return
-	rank1 = ops1[0].rank
-	rank2 = ops2[0].rank
-	# assert rank1 > rank2, f"rank1 should be greater than rank2, got {rank1} and {rank2}"
-
-	def find_matching_op(op):
-		"""
-		Find the corresponding recv or send on the other rank
-		"""
-		for other in ops2:
-			if op.mb_id == other.mb_id and op.op == matching(other.op) and get_peer(other) == op.block_id:
-				return other
-		return None
-
-	assert len(ops1) == len(ops2), (
-		f"ops1 and ops2 should have the same length, got {len(ops1)} and {len(ops2)}"
-	)
-
-	# Find consecutive complementary operations in ops1
-	for i in range(len(ops1) - 1):
-		op1 = ops1[i]
-		if op1.op not in comm_types:
-			continue
-		for j in range(i + 1, len(ops2) - 1):
-			op2 = ops1[j]
-			if op2.op not in comm_types:
-				continue
-
-			# If it's already batched, there is no issue here ; we can skip
-			if (
-				op1.options.get(OpOptions.BATCHED_COMM, None) is not None
-				or op2.options.get(OpOptions.BATCHED_COMM, None) is not None
-			):
-				continue
-
-			# Check if operations are complementary
-			if op2.op in complementary(op1.op):
-				op3 = find_matching_op(op2)
-				op4 = find_matching_op(op1)
-
-				if ops2.index(op3) > ops2.index(op4):
-					# Not blocking ! That's the regular (send/recv) (recv/send) pattern
-					continue
-
-				# If op1 and op2 are not batched, op3 and op4 should not be batched either
-				assert (
-					op3.options.get(OpOptions.BATCHED_COMM, None) is None
-					and op4.options.get(OpOptions.BATCHED_COMM, None) is None
-				), "Operations should not be batched already"
-
-				id_ = (rank1, rank2, i + 1, j + 1)  # Unique id for pair + batch
-				if not dist.is_initialized() or dist.get_rank() == 0:
-					logger.debug(
-						f"Marked operations for batched communication: {op1}, {op2}, {op3}, {op4} with id {id_}"
-					)
-				op1.options[OpOptions.BATCHED_COMM] = id_
-				op2.options[OpOptions.BATCHED_COMM] = id_
-				op3.options[OpOptions.BATCHED_COMM] = id_
-				op4.options[OpOptions.BATCHED_COMM] = id_
 
 
 def get_peer(op):
@@ -211,28 +142,6 @@ def get_peer_rank(op, placement):
 	if peer is None:
 		return None
 	return placement[peer]
-
-
-def mark_batched_comms(schedule, placement):
-	"""
-	Finds all operations that need to be batched and marks them for execution
-	"""
-
-	ranks = []
-	# Unique, reverse order
-	for p in reversed(placement):
-		if p not in ranks:
-			ranks.append(p)
-
-	visited = set()
-	for i in ranks:
-		for j in ranks:
-			if i == j or j in visited:
-				continue
-			ops1 = [op for op in schedule if op.rank == i and get_peer_rank(op, placement) == j]
-			ops2 = [op for op in schedule if op.rank == j and get_peer_rank(op, placement) == i]
-			resolve_one_pair(ops1, ops2)
-		visited.add(i)
 
 
 def schedule_to_str(schedule, print_comms=False):
