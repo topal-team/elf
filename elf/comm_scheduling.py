97d2c40b2c54c3c7a3374bd6e575734f06b25b25
Adrien-AM <Adrien-AM@users.noreply.github.com>
2025-06-05 12:04:35 +0000

    Apply automatic formatting and linting fixes

--- elf/comm_scheduling.py
+++ elf/comm_scheduling.py
@@ -2,178 +2,177 @@
 Static analysis of communications to avoid deadlocks, using a dependency graph representation of the schedule.
 """
 
-
 from typing import List, Tuple, Set, Dict
 from collections import defaultdict
 
 from .scheduling import OperationType, get_peer, matching, comm_types, Operation
 
-class DirectedGraph:
-    """
-    A simple directed graph structure.
-    """
-    
-    def __init__(self, num_nodes: int):
-        """
-        Initialize a directed graph with the specified number of nodes.
-        
-        :param num_nodes: Number of nodes in the graph
-        :type num_nodes: int
-        """
-        if num_nodes <= 0:
-            raise ValueError("Number of nodes must be positive")
-        
-        self.num_nodes = num_nodes
-        self.adjacency_list: Dict[int, List[int]] = defaultdict(list)
-        self.reverse_adjacency_list: Dict[int, List[int]] = defaultdict(list)
-        self.edges: Set[Tuple[int, int]] = set()
-    
-    def add_edge(self, source: int, target: int) -> None:
-        """
-        Add an edge from source to target.
-        
-        :param source: Source node
-        :type source: int
-        :param target: Target node
-        :type target: int
-        """
-        if not (0 <= source < self.num_nodes and 0 <= target < self.num_nodes):
-            raise ValueError("Node indices must be within valid range")
-        
-        if source == target:
-            raise ValueError("Self-loops are not allowed")
-        
-        if (source, target) not in self.edges:
-            self.edges.add((source, target))
-            self.adjacency_list[source].append(target)
-            self.reverse_adjacency_list[target].append(source)
-    
-    def remove_edge(self, source: int, target: int) -> None:
-        """
-        Remove an edge from source to target.
-        
-        :param source: Source node
-        :type source: int
-        :param target: Target node
-        :type target: int
-        """
-        if (source, target) in self.edges:
-            self.edges.remove((source, target))
-            self.adjacency_list[source].remove(target)
-            self.reverse_adjacency_list[target].remove(source)
-    
-    def get_successors(self, node: int) -> List[int]:
-        """
-        Get all successors of a node.
-        
-        :param node: Node to get successors for
-        :type node: int
-        :return: List of successor nodes
-        :rtype: List[int]
-        """
-        return self.adjacency_list[node].copy()
-    
-    def get_predecessors(self, node: int) -> List[int]:
-        """
-        Get all predecessors of a node.
-        
-        :param node: Node to get predecessors for
-        :type node: int
-        :return: List of predecessor nodes
-        :rtype: List[int]
-        """
-        return self.reverse_adjacency_list[node].copy()
-    
-    def has_edge(self, source: int, target: int) -> bool:
-        """
-        Check if an edge exists from source to target.
-        
-        :param source: Source node
-        :type source: int
-        :param target: Target node
-        :type target: int
-        :return: True if edge exists, False otherwise
-        :rtype: bool
-        """
-        return (source, target) in self.edges
-    
-    def has_unidirectional_edge(self, node1: int, node2: int) -> bool:
-        """
-        Check if there is a unidirectional edge between two nodes.
-        """
-        return self.has_edge(node1, node2) and not self.has_edge(node2, node1)
-    
-    def has_bidirectional_edge(self, node1: int, node2: int) -> bool:
-        """
-        Check if there is a bidirectional edge between two nodes.
-        """
-        return self.has_edge(node1, node2) and self.has_edge(node2, node1)
-    
-    def get_in_degree(self, node: int) -> int:
-        """
-        Get the in-degree of a node.
-        
-        :param node: Node to get in-degree for
-        :type node: int
-        :return: In-degree of the node
-        :rtype: int
-        """
-        return len(self.reverse_adjacency_list[node])
-    
-    def get_out_degree(self, node: int) -> int:
-        """
-        Get the out-degree of a node.
-        
-        :param node: Node to get out-degree for
-        :type node: int
-        :return: Out-degree of the node
-        :rtype: int
-        """
-        return len(self.adjacency_list[node])
-    
-    def get_all_edges(self) -> List[Tuple[int, int]]:
-        """
-        Get all edges in the graph.
-        
-        :return: List of edges as (source, target) tuples
-        :rtype: List[Tuple[int, int]]
-        """
-        return list(self.edges)
-    
-    def has_cycle(self) -> bool:
-        """
-        Check if the graph has a cycle using DFS, ignoring 2-cycles.
-        
-        :return: True if graph has a cycle of length > 2, False otherwise
-        :rtype: bool
-        """
-        WHITE, GRAY, BLACK = 0, 1, 2
-        color = [WHITE] * self.num_nodes
-        
-        def dfs(node: int, parent: int = -1) -> bool:
-            if color[node] == GRAY:
-                return True  # Back edge found, cycle detected
-            if color[node] == BLACK:
-                return False  # Already processed
-            
-            color[node] = GRAY
-            for successor in self.get_successors(node):
-                # Skip 2-cycles: if successor is parent and there's a back edge, it's a 2-cycle
-                if successor == parent and self.has_edge(successor, node):
-                    continue
 
-                if dfs(successor, node):
-                    return True
-            color[node] = BLACK
-            return False
-        
-        for i in range(self.num_nodes):
-            if color[i] == WHITE:
-                if dfs(i):
-                    return True
-        
-        return False
-    
+class DirectedGraph:
+	"""
+	A simple directed graph structure.
+	"""
+
+	def __init__(self, num_nodes: int):
+		"""
+		Initialize a directed graph with the specified number of nodes.
+
+		:param num_nodes: Number of nodes in the graph
+		:type num_nodes: int
+		"""
+		if num_nodes <= 0:
+			raise ValueError("Number of nodes must be positive")
+
+		self.num_nodes = num_nodes
+		self.adjacency_list: Dict[int, List[int]] = defaultdict(list)
+		self.reverse_adjacency_list: Dict[int, List[int]] = defaultdict(list)
+		self.edges: Set[Tuple[int, int]] = set()
+
+	def add_edge(self, source: int, target: int) -> None:
+		"""
+		Add an edge from source to target.
+
+		:param source: Source node
+		:type source: int
+		:param target: Target node
+		:type target: int
+		"""
+		if not (0 <= source < self.num_nodes and 0 <= target < self.num_nodes):
+			raise ValueError("Node indices must be within valid range")
+
+		if source == target:
+			raise ValueError("Self-loops are not allowed")
+
+		if (source, target) not in self.edges:
+			self.edges.add((source, target))
+			self.adjacency_list[source].append(target)
+			self.reverse_adjacency_list[target].append(source)
+
+	def remove_edge(self, source: int, target: int) -> None:
+		"""
+		Remove an edge from source to target.
+
+		:param source: Source node
+		:type source: int
+		:param target: Target node
+		:type target: int
+		"""
+		if (source, target) in self.edges:
+			self.edges.remove((source, target))
+			self.adjacency_list[source].remove(target)
+			self.reverse_adjacency_list[target].remove(source)
+
+	def get_successors(self, node: int) -> List[int]:
+		"""
+		Get all successors of a node.
+
+		:param node: Node to get successors for
+		:type node: int
+		:return: List of successor nodes
+		:rtype: List[int]
+		"""
+		return self.adjacency_list[node].copy()
+
+	def get_predecessors(self, node: int) -> List[int]:
+		"""
+		Get all predecessors of a node.
+
+		:param node: Node to get predecessors for
+		:type node: int
+		:return: List of predecessor nodes
+		:rtype: List[int]
+		"""
+		return self.reverse_adjacency_list[node].copy()
+
+	def has_edge(self, source: int, target: int) -> bool:
+		"""
+		Check if an edge exists from source to target.
+
+		:param source: Source node
+		:type source: int
+		:param target: Target node
+		:type target: int
+		:return: True if edge exists, False otherwise
+		:rtype: bool
+		"""
+		return (source, target) in self.edges
+
+	def has_unidirectional_edge(self, node1: int, node2: int) -> bool:
+		"""
+		Check if there is a unidirectional edge between two nodes.
+		"""
+		return self.has_edge(node1, node2) and not self.has_edge(node2, node1)
+
+	def has_bidirectional_edge(self, node1: int, node2: int) -> bool:
+		"""
+		Check if there is a bidirectional edge between two nodes.
+		"""
+		return self.has_edge(node1, node2) and self.has_edge(node2, node1)
+
+	def get_in_degree(self, node: int) -> int:
+		"""
+		Get the in-degree of a node.
+
+		:param node: Node to get in-degree for
+		:type node: int
+		:return: In-degree of the node
+		:rtype: int
+		"""
+		return len(self.reverse_adjacency_list[node])
+
+	def get_out_degree(self, node: int) -> int:
+		"""
+		Get the out-degree of a node.
+
+		:param node: Node to get out-degree for
+		:type node: int
+		:return: Out-degree of the node
+		:rtype: int
+		"""
+		return len(self.adjacency_list[node])
+
+	def get_all_edges(self) -> List[Tuple[int, int]]:
+		"""
+		Get all edges in the graph.
+
+		:return: List of edges as (source, target) tuples
+		:rtype: List[Tuple[int, int]]
+		"""
+		return list(self.edges)
+
+	def has_cycle(self) -> bool:
+		"""
+		Check if the graph has a cycle using DFS, ignoring 2-cycles.
+
+		:return: True if graph has a cycle of length > 2, False otherwise
+		:rtype: bool
+		"""
+		WHITE, GRAY, BLACK = 0, 1, 2
+		color = [WHITE] * self.num_nodes
+
+		def dfs(node: int, parent: int = -1) -> bool:
+			if color[node] == GRAY:
+				return True  # Back edge found, cycle detected
+			if color[node] == BLACK:
+				return False  # Already processed
+
+			color[node] = GRAY
+			for successor in self.get_successors(node):
+				# Skip 2-cycles: if successor is parent and there's a back edge, it's a 2-cycle
+				if successor == parent and self.has_edge(successor, node):
+					continue
+
+				if dfs(successor, node):
+					return True
+			color[node] = BLACK
+			return False
+
+		for i in range(self.num_nodes):
+			if color[i] == WHITE:
+				if dfs(i):
+					return True
+
+		return False
 
 
 def topological_sort(graph: DirectedGraph) -> List[int]:
@@ -232,7 +231,9 @@ def topological_sort(graph: DirectedGraph) -> List[int]:
 		if node in pairings:
 			process_node(pairings[node])
 
-	assert len(result) == graph.num_nodes, f"Not all nodes were visited ({len(result)}/{graph.num_nodes})"
+	assert len(result) == graph.num_nodes, (
+		f"Not all nodes were visited ({len(result)}/{graph.num_nodes})"
+	)
 
 	return result
 
